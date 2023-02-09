424cf3b34e729557d4cb1e9b14152532bb9db5a9
Kushian01100111 <comaspedro6@gmail.com>
2023-02-09 11:03:46 -0300

    09/02/2023-2

---
+++ square.js
@@ -0,0 +1,11 @@
+let isSquare = function(n){
+    return Math.sqrt(n) % 1 === 0 ? true: false
+}
+
+
+console.log(isSquare(-1), false)
+console.log(isSquare(25), true)
+console.log(isSquare( 3), false)
+console.log(isSquare( 4), true)
+console.log(isSquare(26), false)
+
